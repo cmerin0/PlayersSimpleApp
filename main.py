bf6e9d310dea4206c3cf7462b9d620c7954f875f
cmerin0 <carlosmerino96@outlook.com>
2025-03-14 00:08:33 -0400

    Fixing environment variables in ci pipeline

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ load_dotenv()
 
 app = create_app()
 
+debug = os.getenv('DEBUG') or True
+port = os.getenv('APP_PORT') or 5000
+
 if __name__ == '__main__':
     app.run(debug=os.getenv('DEBUG'), port=os.getenv('APP_PORT'))
